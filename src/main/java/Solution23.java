7c3bd905c69ff07ee5628f56155bee10a96cbc2f
Patrick <patrickbrookshire@Knights.ucf.edu>
2021-09-19 21:06:56 -0400

    Commit 1

---
+++ src/main/java/Solution23.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Patrick Brookshire
+ */
+
+import java.util.Scanner;
+
+public class Solution23 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        char choice;
+
+        System.out.print("Is the car silent when you turn the key? ");
+
+        choice = input.next().charAt(0);
+
+        
+}
